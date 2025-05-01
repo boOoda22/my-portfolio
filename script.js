7aed629b4f670d59899378f2b6538df6cbcf2a9b
boOoda22 <bodybadr220@gmail.com>
2025-05-01 03:25:40 +0300

    New Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+AOS.init();
+
+const toggleBtn = document.getElementById("toggle-dark");
+toggleBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("light-mode");
+});
